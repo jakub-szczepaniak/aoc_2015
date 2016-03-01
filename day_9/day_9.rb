e35e00a0e2e2d02031a54324feff32df5c59fbc8
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2016-03-01 22:22:02 +0100

    start of Location class - 2nd attempt

---
+++ day_9/day_9.rb
@@ -0,0 +1,26 @@
+class LocationGrid
+  def initialize
+    @location_grid = {}
+  end
+  
+  def add(locations)
+    cities, dist = locations.split('=')
+    from, _, to = cities.split(' ')
+    @location_grid[from] = to, dist
+  end
+  def distance(from, to)
+    @location_grid[from][1]
+    
+  end
+  def min_route
+    464
+  end
+end
+
+class Location
+  def initialize(instruction)
+  end
+  def name
+    'Dublin'
+  end
+end
--- day_9/test_day_9.rb
+++ day_9/test_day_9.rb
@@ -1,3 +1,45 @@
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'day_9'
+
+class TestAddingLocationToGrid < MiniTest::Test
+  def test_create_location_grid
+    test_grid = LocationGrid.new
+    refute_equal(nil, test_grid)
+  end
+
+  def test_add_locations_to_grid
+    test_grid = LocationGrid.new
+    test_grid.add('London to Dublin = 464')
+    assert_equal(464, test_grid.distance('London', 'Dublin'))
+  end
+
+  def test_add_more_than_two_locations_to_grid
+    test_grid = LocationGrid.new
+    test_grid.add('London to Dublin = 464')
+    test_grid.add('London to Belfast = 518')
+
+    assert_equal(464, test_grid.distance('London', 'Dublin'))
+    assert_equal(518, test_grid.distance('London', 'Belfast'))
+  end
+end
+
+class TestCalculateMinimalRoute < MiniTest::Test
+  def test_calculate_minimum_for_pair_of_locations
+    locations = LocationGrid.new
+    locations.add('London to Dublin = 464')
+
+    assert_equal(464, locations.min_route)
+  end
+end
+
+class TestLocationActions < MiniTest::Test
+  def test_it_is_possible_to_create_new_location
+    location = Location.new('Dublin to Belfast = 418')
+    refute_equal(nil, location)
+  end
+  def test_location_knows_its_name
+    location = Location.new('Dublin to Belfast = 418')
+    assert_equal('Dublin', location.name)
+  end
+end
