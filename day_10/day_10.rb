2291a82449175a3abacfbc982c197184b88b4107
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2016-03-04 19:48:21 +0100

    solution to day 10

--- day_10/day_10.rb
+++ day_10/day_10.rb
@@ -1,19 +1,26 @@
 class SequenceCalculator
   def initialize(initial_sequence)
-    fail(StandardError) if valid?(initial_sequence)
+    fail(StandardError) if invalid?(initial_sequence)
     @sequence = initial_sequence.chars.map(&:to_i)
   end
 
-  def calculate
-    prev = @sequence[0]
-    @sequence.slice_before {|e|
-      prev, prev2 = e, prev
-      prev2 != e}.flat_map{ |numbers| [numbers.count, numbers.first]}.join('')
+  def calculate(iterations = 1)
+    iterations.times do
+      prev = @sequence[0]
+      @sequence = @sequence.slice_before { |e|
+        prev, prev2 = e, prev
+        prev2 != e }.flat_map { |numbers| [numbers.count, numbers.first]}
+    end
+    @sequence.join('')
+  end
+
+  def length
+    @sequence.count
   end
 
   private
 
-  def valid?(sequence)
+  def invalid?(sequence)
     sequence == '' || sequence =~ /[\D]/
   end
 end
