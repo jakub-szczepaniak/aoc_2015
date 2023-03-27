906a975165415a67fe4347d3f2a424fb72d0a00c
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-03-27 22:38:34 +0200

    Solution to part 2 day 12.

--- advent/day_12.py
+++ advent/day_12.py
@@ -1,10 +1,22 @@
 import json
 
+def sum_numbers(data, skip=None):
+    if isinstance(data, int):
+        return data
+    elif isinstance(data, list):
+        return sum(sum_numbers(x, skip) for x in data)
+    elif isinstance(data, dict):
+        if skip is not None and skip in data.values():
+            return 0
+        return sum(sum_numbers(x, skip) for x in data.values())
+    else:
+        return 0
+
 def part_1(data):
-    pass
+    return sum_numbers(data)
 
 def part_2(data):
-    pass
+    return sum_numbers(data, 'red')
 
 def main():
     with open('inputs/day_12.json', '+r') as f:
@@ -12,8 +24,6 @@ def main():
     
     print("Part 1: ", part_1(data))
     print("Part 2: ", part_2(data))
-    print(data)
-    print(len(data))
 
 if __name__ == '__main__':
     main()
