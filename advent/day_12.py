20417f55f3c89c8dafccf56f762f330cd11a10c9
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-03-27 22:29:15 +0200

    Adds functions for part one and two.

--- advent/day_12.py
+++ advent/day_12.py
@@ -1,5 +1,19 @@
+import json
+
+def part_1(data):
+    pass
+
+def part_2(data):
+    pass
+
 def main():
-    print("Day 12")
+    with open('inputs/day_12.json', '+r') as f:
+        data = json.load(f)
+    
+    print("Part 1: ", part_1(data))
+    print("Part 2: ", part_2(data))
+    print(data)
+    print(len(data))
 
 if __name__ == '__main__':
     main()
