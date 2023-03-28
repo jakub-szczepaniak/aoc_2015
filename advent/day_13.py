cff4672c154d07146189a9cbe4ff4f401021e07b
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-03-28 22:35:05 +0200

    Adds first test and implements the calculation for optimal arrangement.

--- advent/day_13.py
+++ advent/day_13.py
@@ -1,7 +1,23 @@
 
-def parse_line(line):
-    return {}
+from itertools import permutations
 
+
+def optimal_happiness(happiness_data):
+    guests = list(happiness_data.keys())
+
+    def calculate_happiness(arrangement):
+        happiness = 0
+        for i, guest in enumerate(arrangement):
+            left_neighbour = arrangement[i - 1]
+            right_neighbour = arrangement[(i + 1) % len(arrangement)]
+            happiness += happiness_data[guest][left_neighbour] + happiness_data[guest][right_neighbour] 
+        return happiness
+    
+    max_happiness = float('-inf')
+    for arrangement in permutations(guests):
+        happiness = calculate_happiness(arrangement)
+        max_happiness = max(max_happiness, happiness)
+    return max_happiness
 
 def main():
     with open('inputs/day_13.txt', '+r') as f:
--- tests/test_day_13.py
+++ tests/test_day_13.py
@@ -2,15 +2,13 @@ import pytest
 
 import day_13
 
-@pytest.fixture
-def single_line():
-    return 'Alice would gain 54 happiness units by sitting next to Bob.'
-
-
-
-def test_single_line_to_dict(single_line):
-    assert day_13.parse_line(single_line) == {
-        'Alice': {
-            'Bob': 54
-        }
+def test_optimal_happiness():
+    happiness_data = {
+        "Alice": {"Bob": 54, "Carol": -79, "David": -2},
+        "Bob": {"Alice": 83, "Carol": -7, "David": -63},
+        "Carol": {"Alice": -62, "Bob": 60, "David": 55},
+        "David": {"Alice": 46, "Bob": -7, "Carol": 41}
     }
+    
+    assert day_13.optimal_happiness(happiness_data) == 330
+
