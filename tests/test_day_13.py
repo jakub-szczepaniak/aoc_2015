4fa12ca6779b662c15423d5e0505eb9eb3a52b05
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-03-29 21:58:43 +0200

    Adds one additional test case for optimal_happines.

--- tests/test_day_13.py
+++ tests/test_day_13.py
@@ -2,13 +2,25 @@ import pytest
 
 import day_13
 
-def test_optimal_happiness():
-    happiness_data = {
+happiness_data_1 = {
         "Alice": {"Bob": 54, "Carol": -79, "David": -2},
         "Bob": {"Alice": 83, "Carol": -7, "David": -63},
         "Carol": {"Alice": -62, "Bob": 60, "David": 55},
         "David": {"Alice": 46, "Bob": -7, "Carol": 41}
     }
-    
-    assert day_13.optimal_happiness(happiness_data) == 330
+
+happiness_data_2 = {
+        "A": {"B": 10, "C": 20},
+        "B": {"A": 30, "C": 40},
+        "C": {"A": 50, "B": 60}
+}
+
+@pytest.fixture(params=[(happiness_data_1, 330), (happiness_data_2, 210)])
+def happiness_data(request):
+    return request.param
+
+def test_optimal_happiness(happiness_data):
+    input, expected = happiness_data
+    assert day_13.optimal_happiness(input) == expected
+
 
